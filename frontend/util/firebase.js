0cac6fb22b96e20d242e0ce9bbf70ff9c54f91b6
tsukii21 <jaidevkr2@gmail.com>
2021-11-21 13:33:36 +0530

    fixed some styles and testing analytics

--- frontend/util/firebase.js
+++ frontend/util/firebase.js
@@ -1,14 +1,13 @@
 // Import the functions you need from the SDKs you nee
-import firebase from "firebase/app";
-import 'firebase/analytics';
+import firebase from "firebase/app"
+import "firebase/analytics"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig;
+var firebaseConfig
 
-
-if (process.env.NODE_ENV === 'production') {
-  // -- production project  
+if (process.env.NODE_ENV === "production") {
+  // -- production project
   firebaseConfig = {
     apiKey: "AIzaSyANZ5g3Xsv7zZRbfWwLKBn2hz9vDQLMUns",
     authDomain: "app-challenge-bd473.firebaseapp.com",
@@ -16,11 +15,10 @@ if (process.env.NODE_ENV === 'production') {
     storageBucket: "app-challenge-bd473.appspot.com",
     messagingSenderId: "284829939378",
     appId: "1:284829939378:web:9d0021f267cd77fc987bab",
-    measurementId: "G-KR96PMH51Z"
-  };
-
+    measurementId: "G-KR96PMH51Z",
+  }
 } else {
-  // -- staging project 
+  // -- staging project
   firebaseConfig = {
     apiKey: "AIzaSyCcTaMpYdF9f8UqX5Y8wgalIr3r8PtXmD4",
     authDomain: "app-staging-challenge.firebaseapp.com",
@@ -28,18 +26,15 @@ if (process.env.NODE_ENV === 'production') {
     storageBucket: "app-staging-challenge.appspot.com",
     messagingSenderId: "806418029869",
     appId: "1:806418029869:web:a6d7e70fdc596a195e47a4",
-    measurementId: "G-6WR8L7DED4"
-  };
-
+    measurementId: "G-6WR8L7DED4",
+  }
 }
-
 
 // Initialize Firebase
 
-if (typeof window !== 'undefined' && !firebase.apps.length ) {
-    firebase.initializeApp(firebaseConfig);
-    if ('measurementId' in firebaseConfig) firebase.analytics(); 
+if (typeof window !== "undefined" && !firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+  if ("measurementId" in firebaseConfig) firebase.analytics()
 }
 
-  
-export default firebase ;
+export default firebase
